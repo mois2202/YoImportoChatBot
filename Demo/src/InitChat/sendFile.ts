b3e2f673a99804587fb9f257d5d42985ac25736c
Moises Ochoa <moisesochoa2202@gmail.com>
2024-11-03 19:20:55 -0400

    Mensaje generico con multimedia

---
+++ Demo/src/InitChat/sendFile.ts
@@ -0,0 +1,6 @@
+
+export const sendFile = (mediaPath : string, body? : string,) : {body?: string; mediaPath: string}[] => {
+    const ImageToSend =
+    [{body, mediaPath}]
+    return ImageToSend;
+}
--- Demo/src/shared/db/dbConfig.ts
+++ Demo/src/shared/db/dbConfig.ts
@@ -2,8 +2,8 @@ export const dbConfig = {
     "db": {
       "host_pg": "localhost",
       "user_pg": "postgres",
-      "password_pg": "Sa753951",
-      "database_pg": "BdBot",
+      "password_pg": "1234",
+      "database_pg": "DemoChatBot",
       "port_pg": 5432
     }
   }
